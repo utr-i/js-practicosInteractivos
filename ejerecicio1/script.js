afa1a3fa64ea900a7319a3bcf0489e133876fe78
Francisco Joaquin Elisondo <f.elisondo@itecriocuarto.org.ar>
2025-04-04 11:36:24 -0300

    Ejercicio 1 Resuelto

--- ejerecicio1/script.js
+++ ejerecicio1/script.js
@@ -1,21 +1,29 @@
-// TODO: Pedir el nombre con prompt
-// TODO: Pedir la edad
-// TODO: Calcular el precio de la entrada según la edad
-// TODO: Mostrar un mensaje personalizado con alert
+// Ejercicio 1
+// Menor 13 1000
+// Entre 13 y 17 1500
+// Mayor 17 2000
+// Menor a 5 o mayor a 80 Gratis
 
-// Ejemplo final esperado:
-// "Hola Juan, tenés 16 años. Tu entrada cuesta $1500"
-
-const nombre = prompt("Ingrese su nombre").toLowerCase()
-alert("Su nombre es:" + nombre)
-const edad = parseInt(prompt("Ingrese su edad"))
-nombre.toLowerCase()
-if (edad === 18) {
-    alert("Su edad es 18")
-} else if (edad === 17) {
-    alert("Su edad es 17")
-} else {
+const edad = parseInt(prompt("Ingrese su edad: "))
+//Gratis
+if (edad <= 5 || edad >= 80) {
+    alert("Su entrada es gratis")
+}
+// Menor 13
+else if (edad <= 12) {
+    alert("Su entrada cuesta $1000")
+}
+//Entre 13 y 17
+else if (edad >= 13 && edad <= 17) {
+    alert("Su entrada cuesta $1500")
+}
+//Mayor 17
+else if (edad >= 18) {
+    alert("Su entrada cuesta $2000")
+}
+//Desconocido
+else {
     alert("Desconozco su edad")
 }
 
-console.log(nombre);
+console.log(edad);
